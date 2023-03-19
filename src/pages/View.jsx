9716bad277c2d659928c67bfc03bb94978f78c24
Gabriel Csapo <gabecsapo@gmail.com>
2023-03-19 14:30:07 -0700

    fix(bug): centering issue when clicking into a model

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -15,60 +15,71 @@ const STLViewer = ({ stlFiles }) => {
 
   useEffect(() => {
     const mount = mountRef.current;
-    let renderer, camera, scene, geometry, material, mesh, controls;
+const canvasAspect = mount.clientWidth / mount.clientHeight;
+let renderer, camera, scene, geometry, material, mesh, controls;
 
-    const loader = new STLLoader();
+const loader = new STLLoader();
 
-    const absolutePath = `file://${stlFilePath}`;
+const absolutePath = `file://${stlFilePath}`;
 
-    camera = new THREE.PerspectiveCamera(
-      75,
-      mount.clientWidth / mount.clientHeight,
-      0.1,
-      1000
-    );
+// Calculate the aspect ratio based on the canvas size
+camera = new THREE.PerspectiveCamera(
+  75,
+  canvasAspect,
+  0.1,
+  1000
+);
 
-    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: false });
-    renderer.setClearColor(0xffffff, 0);
-    mount.appendChild(renderer.domElement);
-    rendererRef.current = renderer;
+renderer = new THREE.WebGLRenderer({ alpha: true, antialias: false });
+renderer.setClearColor(0xffffff, 0);
+mount.appendChild(renderer.domElement);
+rendererRef.current = renderer;
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
-    controls.enableZoom = true;
+controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
 
-    const animate = () => {
-      requestRef.current = requestAnimationFrame(animate);
+const animate = () => {
+  requestRef.current = requestAnimationFrame(animate);
 
-      controls.update();
+  controls.update();
 
-      renderer.render(scene, camera);
-    };
+  renderer.render(scene, camera);
+};
 
-    loader.load(absolutePath, (geometry) => {
-      material = new THREE.MeshNormalMaterial();
-      mesh = new THREE.Mesh(geometry, material);
-      scene = new THREE.Scene();
-      scene.add(mesh);
+loader.load(absolutePath, (geometry) => {
+  geometry.center();
+  material = new THREE.MeshNormalMaterial();
+  mesh = new THREE.Mesh(geometry, material);
+  scene = new THREE.Scene();
+  scene.add(mesh);
 
-      const box = new THREE.Box3().setFromObject(mesh);
-      const center = box.getCenter(new THREE.Vector3());
-      const size = box.getSize(new THREE.Vector3());
+  const box = new THREE.Box3().setFromObject(mesh);
+  const center = box.getCenter(new THREE.Vector3());
+  const size = box.getSize(new THREE.Vector3());
 
-      const maxSize = Math.max(size.x, size.y, size.z);
-      const fitHeightDistance =
-        maxSize / (2 * Math.atan((Math.PI * camera.fov) / 360));
-      camera.aspect = mount.clientWidth / mount.clientHeight;
-      camera.far = fitHeightDistance * 4;
-      camera.near = fitHeightDistance / 4;
-      camera.position.set(center.x, center.y, fitHeightDistance * 2.5);
-      camera.lookAt(center);
+  const maxSize = Math.max(size.x, size.y, size.z);
+  const fitHeightDistance =
+    maxSize / (2 * Math.atan((Math.PI * camera.fov) / 360));
+  const aspectRatio = canvasAspect; // Use the canvas aspect ratio
+  camera.aspect = aspectRatio;
 
-      renderer.setSize(mount.clientWidth, mount.clientHeight);
+  // Set the distance between the camera and the center of the bounding box
+  const distance = fitHeightDistance + maxSize / 4;
 
-      animate();
-    });
+  // Position the camera at the center of the bounding box, looking towards it
+  camera.position.set(center.x, center.y, center.z + distance);
+  camera.lookAt(center);
+
+  camera.far = distance * 2; // Adjust the far plane of the camera
+  camera.near = distance / 100; // Adjust the near plane of the camera
+
+  renderer.setSize(mount.clientWidth, mount.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  animate();
+});
 
     return () => {
       cancelAnimationFrame(requestRef.current);
@@ -82,7 +93,7 @@ const STLViewer = ({ stlFiles }) => {
   return (
     <>
       <br />
-      <div style={{ width: "100vw", height: "500px" }} ref={mountRef} />
+      <div style={{ width: "300px", height: "250px", margin: '0 auto' }} ref={mountRef} />
       <hr />
       {found ? (
         <div>
